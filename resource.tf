3b40e1c47356d08d3141fd1586a5d414c049a08e
HameedNazeer <122872561+HameedNazeer@users.noreply.github.com>
2023-04-09 18:05:05 +0530

    Update resource.tf

--- resource.tf
+++ resource.tf
@@ -2,31 +2,24 @@
 variable "aws_region" {
   default = "ap-south-1"
 }
-
 variable "instance_name" {
   default = "deploy to test"
 }
-
 variable "key_name" {
   default = "mumbai.pem"
 }
-
 variable "security_group_id" {
-  default = "sg-0e2c22e36dc10d118"
+  default = "sg-09947aa36d4d8ebb2"
 }
-
 variable "vpc_id" {
   default = "vpc-0e7f18d86bac2affe"
 }
-
 variable "instance_type" {
   default = "t2.micro"
 }
-
 variable "ami_id" {
   default = "ami-02eb7a4783e7e9317"
 }
-
 # Define EC2 instance resource
 resource "aws_instance" "ec2_instance" {
   ami           = var.ami_id
